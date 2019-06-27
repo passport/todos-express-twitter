14976215d8b0e2ae5b16bf6745f5f4ae8caebddd
Jared Hanson <jaredhanson@gmail.com>
2019-06-27 13:14:48 -0700

    Update dependencies.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 
 var express = require('express');
 var passport = require('passport');
@@ -62,7 +62,6 @@ app.set('view engine', 'ejs');
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
 app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
