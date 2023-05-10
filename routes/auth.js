44620d063156744d665c16a6e9fab8b9f33afd22
Jared Hanson <jaredhanson@gmail.com>
2023-05-10 06:32:38 -0700

    Update routes.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,60 @@
 var express = require('express');
 var passport = require('passport');
+var TwitterStrategy = require('passport-twitter');
 var db = require('../db');
+
+
+passport.use(new TwitterStrategy({
+  consumerKey: process.env['TWITTER_CONSUMER_KEY'],
+  consumerSecret: process.env['TWITTER_CONSUMER_SECRET'],
+  callbackURL: '/oauth/callback/twitter'
+}, function verify(token, tokenSecret, profile, cb) {
+  db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+    'https://twitter.com',
+    profile.id
+  ], function(err, row) {
+    if (err) { return cb(err); }
+    if (!row) {
+      db.run('INSERT INTO users (name) VALUES (?)', [
+        profile.displayName
+      ], function(err) {
+        if (err) { return cb(err); }
+        var id = this.lastID;
+        db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+          id,
+          'https://twitter.com',
+          profile.id
+        ], function(err) {
+          if (err) { return cb(err); }
+          var user = {
+            id: id,
+            name: profile.displayName
+          };
+          return cb(null, user);
+        });
+      });
+    } else {
+      db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
+        if (err) { return cb(err); }
+        if (!row) { return cb(null, false); }
+        return cb(null, row);
+      });
+    }
+  });
+}));
+
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username, name: user.name });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
 
 var router = express.Router();
 
@@ -8,62 +62,18 @@ router.get('/login', function(req, res, next) {
   res.render('login');
 });
 
-router.get('/login/federated/twitter.com', passport.authenticate('twitter'));
+router.get('/login/federated/twitter', passport.authenticate('facebook'));
 
-router.get('/oauth/callback/twitter',
-  passport.authenticate('twitter', { assignProperty: 'federatedUser', failureRedirect: '/login' }),
-  function(req, res, next) {
-    db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-      'https://twitter.com',
-      req.federatedUser.id
-    ], function(err, row) {
-      if (err) { return next(err); }
-      if (!row) {
-        db.run('INSERT INTO users (name) VALUES (?)', [
-          req.federatedUser.displayName
-        ], function(err) {
-          if (err) { return next(err); }
-          
-          var id = this.lastID;
-          db.run('INSERT INTO federated_credentials (provider, subject, user_id) VALUES (?, ?, ?)', [
-            'https://twitter.com',
-            req.federatedUser.id,
-            id
-          ], function(err) {
-            if (err) { return next(err); }
-            var user = {
-              id: id.toString(),
-              displayName: req.federatedUser.displayName
-            };
-            req.login(user, function(err) {
-              if (err) { return next(err); }
-              res.redirect('/');
-            });
-          });
-        });
-      } else {
-        db.get('SELECT rowid AS id, username, name FROM users WHERE rowid = ?', [ row.user_id ], function(err, row) {
-          if (err) { return next(err); }
-    
-          // TODO: Handle undefined row.
-          var user = {
-            id: row.id.toString(),
-            username: row.username,
-            displayName: row.name
-          };
-          req.login(user, function(err) {
-            if (err) { return next(err); }
-            res.redirect('/');
-          });
-        });
-      }
-    });
-    
+router.get('/oauth/callback/twitter', passport.authenticate('twitter', {
+  successReturnToOrRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+router.post('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
   });
-
-router.get('/logout', function(req, res, next) {
-  req.logout();
-  res.redirect('/');
 });
 
 module.exports = router;
